462c331b0ebad7c8927d9909555edb0e127a2f15
nglong18101994 <nglong18101994@gmail.com>
2019-04-04 14:19:33 +0700

    update api to https

--- src/app/service/service.private.ts
+++ src/app/service/service.private.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class PrivateService {
-	static BASE_DOMAIN = 'http://sdkapi.100d.mobi/';
+	static BASE_DOMAIN = 'https://sdkapi.100d.mobi/';
 	// static BASE_DOMAIN = 'http://dev.sdkapi.100d.mobi/';
 	static BASE_URL = PrivateService.BASE_DOMAIN + 'v1/';
 	static BASE_UPLOAD_URL = PrivateService.BASE_DOMAIN + 'v1/upload';
